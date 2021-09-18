9ee96dd44a6f2a06fd1e3f27bf7600cfd03434c7
Xwilarg <xwilarg@yahoo.fr>
2021-09-18 20:39:34 +0200

    Render gradient

--- ExtendedAvalonia/Slider/GradientSlider.axaml.cs
+++ ExtendedAvalonia/Slider/GradientSlider.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using System.Drawing;
+using System.Linq;
 
 namespace ExtendedAvalonia.Slider
 {
@@ -14,6 +15,45 @@ namespace ExtendedAvalonia.Slider
 
         private bool _isInit;
 
+        private Color GetColorFromPosition(double position)
+        {
+            var downSlider = this.FindControl<ExtendedSlider>("SliderDown");
+            Thumb min, max;
+
+            if (downSlider.Thumbs.Any(x => x.Position <= position)) // We found a slider lower than our value
+            {
+                min = downSlider.Thumbs.Where(x => x.Position <= position).OrderByDescending(x => x.Position).ToArray()[0]; // Closest to our left
+            }
+            else
+            {
+                return downSlider.Thumbs.OrderBy(x => x.Position).ToArray()[0].Color; // Else we can just return the smaller one
+            }
+
+            // Then we do the same for max
+            if (downSlider.Thumbs.Any(x => x.Position >= position))
+            {
+                max = downSlider.Thumbs.Where(x => x.Position >= position).OrderBy(x => x.Position).ToArray()[0];
+            }
+            else
+            {
+                return downSlider.Thumbs.OrderByDescending(x => x.Position).ToArray()[0].Color;
+            }
+
+            if (min.Color == max.Color) // Nothing to do since min and max are same color
+            {
+                return min.Color;
+            }
+
+            var percent = (position - min.Position) / (max.Position - min.Position); // Percent between 0 and 1
+
+            return Color.FromArgb(
+                alpha: 255,
+                red: (int)(percent * min.Color.R + (1 - percent) * max.Color.R),
+                green: (int)(percent * min.Color.G + (1 - percent) * max.Color.G),
+                blue: (int)(percent * min.Color.B + (1 - percent) * max.Color.B)
+            );
+        }
+
         public override void Render(Avalonia.Media.DrawingContext context)
         {
             if (!_isInit)
@@ -25,7 +65,8 @@ namespace ExtendedAvalonia.Slider
                 upSlider.AddThumb(new Thumb() { Position = 1.0, Color = Color.Transparent });
 
                 var downSlider = this.FindControl<ExtendedSlider>("SliderDown");
-                downSlider.AddThumb(new Thumb() { Position = 0.0, Color = Color.Transparent });
+                downSlider.AddThumb(new Thumb() { Position = 0.0, Color = Color.Red });
+                downSlider.AddThumb(new Thumb() { Position = 1.0, Color = Color.Blue });
             }
 
             // Renderer display a big square of our color
@@ -37,7 +78,7 @@ namespace ExtendedAvalonia.Slider
                 data[y] = new int[(int)renderer.Bounds.Width];
                 for (int x = 0; x < (int)renderer.Bounds.Width; x++)
                 {
-                    data[y][x] = Color.Red.ToArgb();
+                    data[y][x] = GetColorFromPosition(x / renderer.Bounds.Width).ToArgb();
                 }
             }
 
