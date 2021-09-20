e712d0fec61fb5c3f0b0d15fa320eaba7a53cd38
Xwilarg <xwilarg@yahoo.fr>
2021-09-20 20:19:36 +0200

    Remove garbage info from PositionColor serialization

--- ExtendedAvalonia/Slider/PositionColor.cs
+++ ExtendedAvalonia/Slider/PositionColor.cs
@@ -4,7 +4,23 @@ namespace ExtendedAvalonia.Slider
 {
     public struct PositionColor
     {
-        public Color Color { init; get; }
+        public Color Color
+        {
+            set
+            {
+                R = value.R;
+                G = value.G;
+                B = value.B;
+            }
+            get
+            {
+                return Color.FromArgb(255, R, G, B);
+            }
+        }
+
+        public byte R { set; get; }
+        public byte G { set; get; }
+        public byte B { set; get; }
         public double Position { init; get; }
     }
 }
